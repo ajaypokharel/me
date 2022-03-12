99b31509ab9e95317d2594d479e0efab165c4b02
Ajay Pokharel <ajaypokharel8@gmail.com>
2022-03-12 15:06:10 -0500

    [update] updates contact headline

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
+      <h2> ☎️ Where to Reach Me ☎️</h2>
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
